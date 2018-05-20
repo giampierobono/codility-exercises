34b02ff9dde956309fec8f3a0b723032fb976213
Giampiero Bono <giampiero.bono@hotmail.it>
2018-05-20 14:40:04 +0200

    * Improve cyclic rotation solution

--- cyclic rotation/cyclic-rotation.js
+++ cyclic rotation/cyclic-rotation.js
@@ -1,22 +1,14 @@
+const shiftRightArray = (array) => {
+    const clone = [...array];
+    clone.unshift(clone.pop());
+    return clone;
+}
+
 const solution = (A, K) => {
    if(A.length === 0 || K === 0){
      return A; 
    }
-
-   let shiftArray = [];
-   
-   const shiftRightArray = (array) => {
-       const clone = [...array];
-       clone.unshift(clone.pop());
-       return clone;
-   }
-   
-   for(let i = 0; i < K ; i++) {
-       if(i === 0) {
-           shiftArray = shiftRightArray(A);
-       } else {
-           shiftArray = shiftRightArray(shiftArray);
-       }
-   }
-   return shiftArray;
+   let shiftedArray = [...A];
+   [...Array(K).keys()].forEach(() => shiftedArray = shiftRightArray(shiftedArray));
+   return shiftedArray;
 }
